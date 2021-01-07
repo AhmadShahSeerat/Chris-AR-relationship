407cdc6248aeb3f54f5e64c9fc7e5fea4fb71a4b
iamShah <formal>
2021-01-07 12:01:25 -0500

    jan 7

--- config/environment.rb
+++ config/environment.rb
@@ -9,10 +9,4 @@ ActiveRecord::Base.establish_connection({
     database: 'db/development.sqlite3'
   })
 
-  echo "# Chris-AR-relationship" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:shah2889/Chris-AR-relationship.git
-git push -u origin main
+ 
